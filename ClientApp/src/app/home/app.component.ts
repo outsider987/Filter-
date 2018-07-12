40187d0c6a62a13a158ab4b2ba66f403dffac3e4
Outsider987 <t7902195204@gmail.com>
2018-07-12 21:19:36 +0800

    update

---
+++ ClientApp/src/app/home/app.component.ts
@@ -0,0 +1,55 @@
+import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgModel } from '@angular/forms';
+import {debounceTime} from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
+
+
+
+@Component({
+  selector: 'home-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  data: any;
+  keyword = '';
+  Zone = '';
+  Infor = '';
+  @ViewChild('tKeyword') tkeyword: NgModel;
+  @ViewChild('tZone') tZone: NgModel;
+  @ViewChild('tInfor') tInfor: NgModel;
+
+  constructor(private http: HttpClient) { }
+   // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit(): void {
+    this.http.get('/api/spots').subscribe((value: any) => {
+      this.data = value;
+    });
+  }
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewInit() {
+
+    combineLatest(
+      this.tkeyword.valueChanges,
+      this.tZone.valueChanges,
+      this.tInfor.valueChanges
+     )
+     .pipe(
+       debounceTime(500)
+      )
+      .subscribe(evt => {
+      const [k, z, y] = evt;
+      this.http.get('api/spots?k=' + k + '&z=' + z + '&y=' + y).subscribe((value: any) => {
+         this.data = value;
+      });
+     });
+
+
+
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+
+  }
+}
